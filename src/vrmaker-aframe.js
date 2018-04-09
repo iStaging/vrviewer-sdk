1644715f43fbed3f9d23297443eade8d1b73ea47
Alex <alexchen.info@gmail.com>
2018-04-09 09:47:17 +0800

    Fix eslint

--- src/vrmaker-aframe.js
+++ src/vrmaker-aframe.js
@@ -1,6 +1,9 @@
 class Aframe {
   constructor () {
     this.generateAframe = function () {
+      // create a-scene
+      // document.createElement
+      // put panoramas inside a-sky
       console.log('generateAframe this.getCurrentPanorama()', this.getCurrentPanorama())
       return this
     }
