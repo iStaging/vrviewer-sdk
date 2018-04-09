9e49f1c45c28c28231a0cd0b65c10ebb2183dcdc
Alex <alexchen.info@gmail.com>
2018-04-09 11:08:54 +0800

    Load aframe successfully

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ import './src'
 // import 'vrmaker'
 // import './src/vrmaker-krpano'
 
+/* eslint-disable */
 VRMaker.init('el', {
   panoramas: [{ id: 'bb' }]
 })
 
-console.log(VRMaker)
-console.log(VRMaker.getCurrentPanorama())
-console.log(VRMaker.generateKrpano())
-console.log(VRMaker.generateAframe())
+// console.log(VRMaker)
+// console.log(VRMaker.getCurrentPanorama())
+// console.log(VRMaker.generateKrpano())
+window.onload = function () {
+  console.log('11:', VRMaker.generateAframe())
+}
--- src/vrmaker-aframe.js
+++ src/vrmaker-aframe.js
@@ -4,7 +4,19 @@ class Aframe {
       // create a-scene
       // document.createElement
       // put panoramas inside a-sky
-      console.log('generateAframe this.getCurrentPanorama()', this.getCurrentPanorama())
+      // console.log('generateAframe this.getCurrentPanorama()', this.getCurrentPanorama())
+      var aSceneEl = document.createElement('a-scene')
+      aSceneEl.style.display = 'block'
+      aSceneEl.style.position = 'relative'
+      aSceneEl.style.width = '100%'
+      aSceneEl.style.height = '100%'
+      var aSkyEl = document.createElement('a-sky')
+      aSkyEl.setAttribute('src', 'https://aframe.io/aframe/examples/boilerplate/panorama/puydesancy.jpg')
+      // asceneEl.style.width = '640px'
+      // asceneEl.style.height = '480px'
+      aSceneEl.appendChild(aSkyEl)
+      document.body.appendChild(aSceneEl)
+      console.log(document.body)
       return this
     }
   }
