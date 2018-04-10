ab57fbe295d951f5b7cdfb8332822e15eb70069d
kevin.hu <>
2018-04-10 15:56:33 +0800

    [vrmaker-sdk] refactor structure - pending

--- index.js
+++ index.js
@@ -3,34 +3,39 @@ import './src'
 // import 'vrmaker'
 // import './src/vrmaker-krpano'
 
-window.onload = function () {
-  /* eslint-disable */
-  VRMaker.init({
-    el: document.getElementById('vrmaker-aframe'),
-    panoramas: [{
-      id: 'YRSqB',
-      // src: 'https://aframe.io/aframe/examples/boilerplate/panorama/puydesancy.jpg',
-      src: 'img/test.jpg',
-      // cameraStartRotation: {
-      //   x: 0,
-      //   y: 360,
-      //   z: 0
-      // },
-      index: 0
-    }, {
-      id: 'Hiazr',
-      src: 'https://aframe.io/aframe/examples/boilerplate/panorama/puydesancy.jpg',
-      index: 1
-    }]
+// window.onload = function () {
+/* eslint-disable */
+VRMaker.init({
+  el: document.getElementById('vrmaker-aframe'),
+  panoramas: [{
+    id: 'YRSqB',
+    // src: 'https://aframe.io/aframe/examples/boilerplate/panorama/puydesancy.jpg',
+    src: 'img/test.jpg',
+    // cameraStartRotation: {
+    //   x: 0,
+    //   y: 360,
+    //   z: 0
+    // },
+    index: 0
+  }, {
+    id: 'Hiazr',
+    src: 'https://aframe.io/aframe/examples/boilerplate/panorama/puydesancy.jpg',
+    index: 1
+  }]
+})
+
+// console.log(VRMaker.)
+// console.log(new VRMaker.aframeViewer().test)
+// console.log(new VRMaker.aframeViewer().generateAframe)
+// console.log(VRMaker.generateAframe)
+// const aframeViewer = new VRMaker.aframeViewer()
+// aframeViewer.generateAframe()
+VRMaker.generateAframe()
+
+setTimeout(() => {
+  VRMaker.selectPanorama('Hiazr')
+  VRMaker.setPanoramaSrc(VRMaker.getCurrentPanorama().src, () => {
+    console.log('loaded')
   })
-
-  console.log(VRMaker)
-  VRMaker.generateAframe()
-
-  setTimeout(() => {
-    VRMaker.selectPanorama('Hiazr')
-    VRMaker.setPanoramaSrc(VRMaker.getCurrentPanorama().src, () => {
-      console.log('loaded')
-    })
-  }, 1000)
-}
+}, 1000)
+// }
