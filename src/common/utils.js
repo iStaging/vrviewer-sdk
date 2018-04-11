c6adec6e7d6587392bcc64b6747fed239b3ecc2e
Alex <alexchen.info@gmail.com>
2018-04-11 18:29:24 +0800

    Make utils tests pass

--- src/common/utils.js
+++ src/common/utils.js
@@ -1,4 +1,4 @@
 export const clone = (object) => JSON.parse(JSON.stringify(object))
-export const push = (x, array) => [...array, x]
-export const unshift = (x, array) => [x, ...array]
+export const push = (array, value) => [...array, value]
+export const unshift = (array, value) => [value, ...array]
 export const remove = (index, array) => [...array.slice(0, index), ...array.slice(index + 1)]
