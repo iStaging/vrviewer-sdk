7b259d754033d95d4f2f20105a5f37d74dd74462
Jason Tseng <tsaistorm@gmail.com>
2018-04-20 17:42:44 +0800

    [feature #2415] add unit test: generateKrpano

--- test/krpanoViewer/index.test.js
+++ test/krpanoViewer/index.test.js
@@ -1,5 +1,6 @@
 import KrpanoViewer from '@/KrpanoViewer'
 import krpanoConstants from '@/krpanoViewer/krpano-constants'
+import krpanoHelpers from '@/krpanoViewer/krpano-helpers'
 
 describe('krpano/index.js', () => {
   let krpanoViewer
@@ -16,15 +17,18 @@ describe('krpano/index.js', () => {
 
   it('generateKrpano should call setKrpanoId, setConfig, initKrpanoVRModeItems, generateXml and embedPano', (done) => {
     jest.setTimeout(2000)
-    const mockCallback = jest.fn()
-    krpanoViewer.generateKrpano({}, mockCallback)
-    const krpanoId = krpanoConstants.getKrpanoId()
-    expect(krpanoId).not.toEqual('')
-    const krpanoXml = krpanoConstants.setKrpanoXml()
-    expect(krpanoXml).not.toEqual('')
-    window.setTimeout(() => {
-      expect(mockCallback).toBeCalled()
-      done()
-    }, 1500)
+    krpanoConstants.setKrpanoId = jest.fn()
+    krpanoHelpers.setConfig = jest.fn()
+    krpanoConstants.initKrpanoVRModeItems = jest.fn()
+    krpanoHelpers.generateXml = jest.fn()
+    krpanoHelpers.embedPano = jest.fn()
+
+    krpanoViewer.generateKrpano({})
+    expect(krpanoConstants.setKrpanoId).toBeCalled()
+    expect(krpanoHelpers.setConfig).toBeCalled()
+    expect(krpanoConstants.initKrpanoVRModeItems).toBeCalled()
+    expect(krpanoHelpers.generateXml).toBeCalled()
+    expect(krpanoHelpers.embedPano).toBeCalled()
+    done()
   })
 })
