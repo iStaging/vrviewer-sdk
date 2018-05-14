bff79e71bddf1172e6df3b0f76403158b8b410af
Jason Tseng <tsaistorm@gmail.com>
2018-05-14 18:18:20 +0800

    [feature #2631] fix example

--- examples/index.js
+++ examples/index.js
@@ -1,7 +1,7 @@
 // Make sure you have already using aframe and vrviewer-sdk js first.
 
 // New and init vrviewer with the element you give and the data(panoramas) which come from vrmaker backend service.
-var vrViewer = new VRViewer()
+var vrViewer = new window.VRViewer()
 
 // axios.get('/api/v1/panorama')
 //   .then(function (response) {
