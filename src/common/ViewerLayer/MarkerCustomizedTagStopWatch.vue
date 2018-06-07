07be0ec281818f07861fd40decdd497be8ecc2b7
Jason Tseng <tsaistorm@gmail.com>
2018-06-07 18:16:04 +0800

    [feature #2821] complete stop watch marker info in mobile

--- src/common/ViewerLayer/MarkerCustomizedTagStopWatch.vue
+++ src/common/ViewerLayer/MarkerCustomizedTagStopWatch.vue
@@ -1,6 +1,45 @@
 <template>
   <div class="vrsdk-marker-info-customized-tag-stop-watch">
-    @@
+    <div class="vrsdk-stop-watch-layout">
+      <div class="vrsdk-stop-watch-area-1">
+        <div class="vrsdk-stop-watch-image-container">
+          <span class="vrsdk-bookmark-yellow vrsdk-text-ml">
+            7.5 折
+          </span>
+          <img
+            class="vrsdk-stop-watch-image"
+            src="http://fs1.shop123.com.tw/300123/upload/product/3001233246pic_big_137732.jpg"
+            alt="假圖">
+        </div>
+      </div>
+      <div class="vrsdk-stop-watch-area-2">
+        <div class="vrsdk-stop-watch-bar">
+          <span>倒數 1 天 23:00:58</span>
+          <span>剩餘 5 件</span>
+        </div>
+        <h4 class="vrsdk-stop-watch-title">
+          一二三四五六七八九十一二三四五六七八九十一二三四五六七八九十
+        </h4>
+        <div class="vrsdk-stop-watch-text vrsdk-stop-watch-price vrsdk-text-center">
+          <del>$ 4000</del>
+          <strong class="vrsdk-text-ml">$ 3200</strong>
+        </div>
+        <div class="vrsdk-stop-watch-button-container vrsdk-stop-watch-action vrsdk-text-center">
+          <a
+            href="http://www.google.com.tw"
+            class="vrsdk-stop-watch-button vrsdk-text-ml"
+            target="_blank"
+            rel="noopener">
+            立即搶購
+          </a>
+        </div>
+      </div>
+      <div class="vrsdk-stop-watch-area-3">
+        <div class="vrsdk-stop-watch-description">
+          <pre class="vrsdk-pre-custom">一二三四五六七八九十一二三四五六七八九十一二三四五六七八九十一二三四五六七八九十一二三四五六七八九十一二三四五六七八九十一二三四五六七八九十一二三四五六七八九十一二三四五六七八九十一二三四五六七八九十一二三四五六七八九十一二三四五六七八九十一二三四五六七八九十一二三四五六七八九十一二三四五六七八九十一二三四五六七八九十一二三四五六七八九十一二三四五六七八九十一二三四五六七八九十一二三四五六七八九十一二三四五六七八九十一二三四五六七八九十一二三四五六七八九十一二三四五六七八九十一二三四五六七八九十一二三四五六七八九十一二三四五六七八九十一二三四五六七八九十一二三四五六七八九十一二三四五六七八九十</pre>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -18,7 +57,82 @@ export default {
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus" scoped>
-// @import '~css/variables.styl'
-// @import '~css/extends.styl'
+@import '~css/variables.styl'
+@import '~css/extends.styl'
 
+a,
+span,
+h4 {
+  text-shadow: none
+}
+
+strong {
+  color: $red3
+}
+
+.vrsdk-stop-watch-image-container {
+  position: relative
+  text-align: center
+}
+
+.vrsdk-stop-watch-image {
+  width: 300px
+  max-width: 100%
+}
+
+.vrsdk-bookmark-yellow {
+  position: absolute
+  text-shadow: none
+  color: $red
+  background-color: $yellow
+  padding: 7.5px 11px
+  top: 7px
+  display: inline-flex
+}
+
+.vrsdk-stop-watch-layout {
+  display: flex
+  flex-direction: column
+
+  .vrsdk-stop-watch-area-1 {
+    order: 2
+  }
+
+  .vrsdk-stop-watch-area-2 {
+    order: 1
+  }
+
+  .vrsdk-stop-watch-area-3 {
+    order: 3
+  }
+}
+
+.vrsdk-stop-watch-bar {
+  padding: 0 10px
+  background-color: $yellow
+  display: flex
+  justify-content: space-between
+
+  span {
+    color: $black
+  }
+}
+
+.vrsdk-stop-watch-title {
+  margin-top: 10px
+  margin-bottom: 10px
+}
+
+.vrsdk-stop-watch-button-container {
+  margin-top: 15px
+  margin-bottom: 15px
+}
+
+a.vrsdk-stop-watch-button {
+  display: inline-flex
+  padding: 8px 17px
+  background-color: $red2
+  color: $white
+  border-radius: 5px
+}
 </style>
